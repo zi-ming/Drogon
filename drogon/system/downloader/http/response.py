f04f0ae9546f88893206baf252d0175b30723e07
zi-ming <1564603397@qq.com>
2018-04-02 15:43:52 +0800

    add Response module

---
+++ drogon/system/downloader/http/response.py
@@ -0,0 +1,18 @@
+from requests.models import Response as Response_obj
+
+class Response(object):
+    def __init__(self, response, request):
+        self.response = response
+        self.request = request
+
+    def __str__(self):
+        if not self.response:
+            return "<Response failed:[%s] [%s]>".format(
+                self.response.status.code, self.response.url)
+        if isinstance(self.response, Response_obj):
+            return "<Response [%s] [%s] [%.2f KB]>".format(
+                self.response.status.code, self.response.url, len(self.response.content) / 1000.0)
+        else:
+            return "<Response unknown:[%s] [%s]>".format(
+                self.response.status.code, self.response.url)
+    __repr__ = __str__
