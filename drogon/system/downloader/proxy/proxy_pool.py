935dd01f5d41f6198719b84486c9d12cd2965a4d
zi-ming <1564603397@qq.com>
2018-04-02 16:08:59 +0800

    add proxy_pool module

---
+++ drogon/system/downloader/proxy/proxy_pool.py
@@ -0,0 +1,35 @@
+"""
+    proxy_pool.py
+    ~~~~~~~
+    read proxies from proxy file
+    :author: Max
+    :copyright: (c) 2018
+    :date created: 2018-04-02
+    :python version: 3.6
+"""
+
+import sys
+from drogon.settings.default_settings import PROXY_PATH
+
+class ProxyPool(object):
+    def __init__(self):
+        self._queue = Queue()
+        for line in open(PROXY_PATH):
+            info = line.split(':')
+            if len(info) == 2:
+                proxy = {"http": "http://%s:%s" % (info[0], info[1]),
+                         "https": "http://%s:%s" % (info[0], info[1])}
+            elif len(info) == 4:
+                proxy = {"http": "http://%s:%s@%s:%s/" % (info[2], info[3], info[0], info[1]),
+                         "https": "http://%s:%s@%s:%s/" % (info[2], info[3], info[0], info[1])}
+            else:
+                continue
+            self._queue.put(proxy)
+
+    def __len__(self):
+        return len(self._queue)
+
+    def get(self):
+        proxy = self._queue.get()
+        self._queue.put(proxy)
+        return proxy
