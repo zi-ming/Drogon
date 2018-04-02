2c7620bed3847110ca1a90dab653a0ab9a55f2df
zi-ming <1564603397@qq.com>
2018-04-02 14:26:32 +0800

    add default_settings

---
+++ drogon/settings/default_settings.py
@@ -0,0 +1,15 @@
+import os
+
+BASE_DIR = os.getcwd()
+PROXY_PATH = os.path.join(BASE_DIR, 'proxies.txt')
+
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+
+DEFAULT_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Content-Type": "text/html;charset=utf-8",
+    "Accept-Language":"zh-CN,zh;q=0.8"
+}
