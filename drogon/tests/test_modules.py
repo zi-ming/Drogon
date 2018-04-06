5316a6c02476da93cf8fbc13f1560d18636e21b9
zi-ming <1564603397@qq.com>
2018-04-06 15:24:45 +0800

    add scheduler unittest

--- drogon/tests/test_modules.py
+++ drogon/tests/test_modules.py
@@ -1,20 +1,47 @@
 import unittest2 as unittest
 from drogon.system.downloader.http.request import Request
 from drogon.system.downloader.requests_downloader import RequestsDownloader
+from drogon.system.scheduler.queue import PriorityQueue
 
-class TestDownloader(unittest.TestCase):
-    def test_request_downloader(self):
-        request = Request(
-            url='http://www.cnblogs.com/jhao/p/7243043.html',
+# class TestDownloader(unittest.TestCase):
+#     def test_request_downloader(self):
+#         request = Request(
+#             url='http://www.cnblogs.com/jhao/p/7243043.html',
+#         )
+#         batch = [request,]
+#         downloader = RequestsDownloader()
+#         resps = downloader.download(batch)
+#         for resp in resps:
+#             if not resp.response:
+#                 print('download fail')
+#             else:
+#                 print(resp)
+
+class BaiduSpider(object):
+    spider_id = 'baidu_id'
+    spider_name = 'baidu_name'
+
+    def deom_request(self):
+        return Request(
+            url='http://www.baidu.com',
+            callback=self.demo_func,
+            errback=self.demo_func
         )
-        batch = [request,]
-        downloader = RequestsDownloader()
-        resps = downloader.download(batch)
-        for resp in resps:
-            if not resp.response:
-                print('download fail')
-            else:
-                print(resp)
+
+    def demo_func(self):
+        print('pass test')
+
+class TestQueue(unittest.TestCase):
+    baidu = BaiduSpider()
+    req = baidu.deom_request()
+    queue = PriorityQueue(baidu)
+    queue.push(req)
+    print(len(queue))
+    req = queue.pop()
+    print(req.url)
+    req.callback()
+    req.errback()
+
 
 if __name__ == '__main__':
     unittest.main()
