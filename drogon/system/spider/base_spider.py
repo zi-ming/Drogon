80f9fbf038133268e82e3c73836a7c9c28944db2
zi-ming <1564603397@qq.com>
2018-04-06 20:14:41 +0800

    add base_spider module

---
+++ drogon/system/spider/base_spider.py
@@ -0,0 +1,19 @@
+
+class BaseSpider(object):
+    spider_id = None
+    spider_name = None
+
+    spider_settings = dict(
+        duplicate_remove=False,
+        allow_redirects=False,
+        ignore_error=True,
+        use_proxy=False,
+        timeout=60,
+        priority=0,
+    )
+
+    start_requests = []
+    start_url_file_path = None
+
+    def parse(self, response):
+        print(response)
