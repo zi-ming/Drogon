34cc7c465e4ec52dcf272eb4a765c393c83641a0
zi-ming <1564603397@qq.com>
2018-04-07 15:06:37 +0800

    improve logic

--- drogon/system/utils/engine.py
+++ drogon/system/utils/engine.py
@@ -7,7 +7,6 @@ def read_start_urls_from_file(spider):
     spider_dirname = os.path.join(root, spider.spider_id)
     if not os.path.exists(spider_dirname):
         raise IOError("No such file or directory: '%s'"%(spider_dirname))
-    print(spider_dirname)
     for _, _, files in os.walk(spider_dirname):
         for f in sorted(files):
             with open(os.path.join(spider_dirname, f)) as f:
