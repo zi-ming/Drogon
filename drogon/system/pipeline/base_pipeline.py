435c8f49339d62de77d1909c331237d4840c69fe
zi-ming <1564603397@qq.com>
2018-04-15 14:56:11 +0800

    add notes

--- drogon/system/pipeline/base_pipeline.py
+++ drogon/system/pipeline/base_pipeline.py
@@ -1,3 +1,12 @@
+"""
+    base_pipeline.py
+    ~~~~~~~
+    the Base class of pipeline
+    :author: Max
+    :copyright: (c) 2018
+    :date created: 2018-04-15
+    :python version: 3.6
+"""
 
 class BasePipeline(object):
     def parse(self, response, spider):
