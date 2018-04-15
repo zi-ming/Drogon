5fe23c8e2880c378ea205876d746aa44062b4761
zi-ming <1564603397@qq.com>
2018-04-15 14:57:46 +0800

    optimize code logic

--- drogon/system/result.py
+++ drogon/system/result.py
@@ -1,14 +1,23 @@
+"""
+    result.py
+    ~~~~~~~
+    a result module
+    :author: Max
+    :copyright: (c) 2018
+    :date created: 2018-04-15
+    :python version: 3.6
+"""
 
 import os
 import requests
 from hashlib import md5
-from drogon.settings.default_settings import RESULT_PATH
+from drogon.settings import RESULT_PATH
 
 class Result(object):
     def __init__(self, response, spider, encoding=True):
         self.encoding = 'utf-8'
         if encoding:
-            self.response = self.encode_response(response.response)
+            self.response = response.encode_response(response.response)
         else:
             self.response = response.response
         self.text = self.response.text
@@ -19,20 +28,6 @@ class Result(object):
         self.result_id = md5(self.content).hexdigest()
         path = os.path.join(RESULT_PATH, self.spider.spider_id)
         self.path = os.path.join(path, self.result_id)
-
-
-    def encode_response(self, response):
-        encoding = response.encoding
-        if encoding == 'ISO-8859-1':
-            encodings = requests.utils.get_encodings_from_content(response.text)
-            if encodings:
-                encoding = encodings[0]
-            else:
-                encoding = response.apparent_encoding
-            self.encoding = encoding
-            response.encoding = encoding
-        return response
-
 
     def decode(self, encoding='utf-8'):
         self.content.decode(encoding)
