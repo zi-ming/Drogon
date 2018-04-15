c44e733eefc017efb91da29a500fda95cb756a02
zi-ming <1564603397@qq.com>
2018-04-15 14:50:36 +0800

    add some convenient functions

--- drogon/system/downloader/base_downloader.py
+++ drogon/system/downloader/base_downloader.py
@@ -7,7 +7,20 @@
     :date created: 2018-04-02
     :python version: 3.6
 """
+import requests
+
+from drogon.system.downloader.modules.response import Response
+from drogon.settings import DEFAULT_HEADERS
 
 class BaseDownloader(object):
     def downloader(self, request):
         raise NotImplementedError
+
+    @staticmethod
+    def get(url, headers=None, encoding=True):
+        if not headers:
+            headers = DEFAULT_HEADERS
+        resp = requests.get(url, headers=headers, verify=False)
+        if encoding:
+            resp = Response.encode_response(resp)
+        return resp
