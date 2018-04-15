a36a70a01406d50ac1f83eecb8e55fd624af2c9a
zi-ming <1564603397@qq.com>
2018-04-15 15:03:44 +0800

    reconstraction codes

--- drogon/system/utils/__init__.py
+++ drogon/system/utils/__init__.py
@@ -1,24 +1,32 @@
+"""
+    utils
+    ~~~~~~~
+    commonly used tools
+    :author: Max
+    :copyright: (c) 2018
+    :date created: 2018-04-15
+    :python version: 3.6
+"""
 
 import os
-import logging
+import six
 
-if not os.path.exists('log'):
-    os.mkdir('log')
-logger = logging.getLogger('DROGON')
-logger.setLevel(logging.DEBUG)
+from drogon.settings import START_URLS_PATH
 
-log_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s:%(message)s")
+def to_unicode(text, encoding=None, errors='strict'):
+    if not isinstance(text, bytes):
+        return text
+    if encoding is None:
+        encoding = 'utf-8'
+    return text.decode(encoding, errors)
 
-fh = logging.FileHandler('log/DROGON.log')
-fh.setLevel(logging.INFO)
-fh.setFormatter(log_formatter)
-
-sh = logging.StreamHandler()
-sh.setLevel(logging.DEBUG)
-sh.setFormatter(log_formatter)
-
-logger.addHandler(fh)
-logger.addHandler(sh)
-
-
-
+def read_line_from_start_url_files(spider_id):
+    path = os.path.join(START_URLS_PATH, spider_id)
+    if not os.path.exists(path):
+        return None
+    for _, _, files in os.walk(path):
+        files = sorted(files)
+        for f in files:
+            with open(os.path.join(path, f)) as f:
+                for line in f:
+                    yield line
