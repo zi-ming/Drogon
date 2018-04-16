cc4d48680cfc412fa3c35346a21a6b73e026ca68
zi-ming <1564603397@qq.com>
2018-04-16 23:47:09 +0800

    add parser settings

---
+++ drogon_parser/settings/__init__.py
@@ -0,0 +1,9 @@
+
+import os
+
+BASE_PATH = os.getcwd()
+OUTPUT_PATH = os.path.join(os.path.dirname(BASE_PATH), 'output')
+SAMPLE_PATH = os.path.join(OUTPUT_PATH, 'sample')
+RESULT_PATH = os.path.join(OUTPUT_PATH, 'result')
+SYS_LOG_PATH = os.path.join(OUTPUT_PATH, 'sys_log')
+TASK_LOG_PATH = os.path.join(OUTPUT_PATH, 'task_log')
