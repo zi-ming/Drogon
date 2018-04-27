f388aa878e2527270b9ccce62adf6b7aaad72f69
“youme <1564603397@qq.com>
2018-04-27 16:22:00 +0800

    modify runner

--- drogon_parser/run.py
+++ drogon_parser/run.py
@@ -1,9 +1,9 @@
 
 from drogon_parser.parser_scripts.boss import Parser
-from drogon_parser.parser.base_parser import *
+# from drogon_parser.parser.base_parser import *
 
 if __name__ == '__main__':
-    # parser = Parser()
-    # parser.on_start()
-    parser = Boss() # BaseParser2()
+    parser = Parser()
     parser.on_start()
+    # parser = Boss() # BaseParser2()
+    # parser.on_start()
